6ac292304a438088332ccc8cd35d094dcfd7f389
rinwindows <phurinmeng@gmail.com>
2024-08-13 18:56:55 -0700

    added kdtsuo.github.io and removed reccomendify

---
+++ kdtsuo.github.com/js/style.js
@@ -0,0 +1,5 @@
+// var allClasses = document.getElementsByTagName("div");
+// for (let i = 0; i < allClasses.length; i++) {
+//     let delay = (i * 0.15) + 0.5;
+//     allClasses[i].style.animation = `fadeIn 1s ${delay}s ease forwards`;
+// }
--- recommendify/README.md
+++
@@ -1,1 +0,0 @@
-# A light weight recommendation engine for your past listens on Spotify
