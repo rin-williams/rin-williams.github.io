30f6b58ea1e729b06a27211ff02798ef19bebd73
rinwindows <phurinmeng@gmail.com>
2024-08-15 00:22:02 -0700

    Update website links and remove unused files

--- kdtsuo.github.io/js/style.js
+++
@@ -1,5 +0,0 @@
-// var allClasses = document.getElementsByTagName("div");
-// for (let i = 0; i < allClasses.length; i++) {
-//     let delay = (i * 0.15) + 0.5;
-//     allClasses[i].style.animation = `fadeIn 1s ${delay}s ease forwards`;
-// }
